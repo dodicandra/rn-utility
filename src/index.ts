f541892da01909e788c83c83a0eee3f0bb1be88d
dodicandra <dodicandra20@gmail.com>
2024-11-18 12:15:56 +0700

    export animated box & layout

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Box, type BoxProps, type IStyle as BoxStyle } from './components/box';
+import { AnimatedBox } from './components/box-animated';
+import { BoxAnimatedLayout } from './components/box-animated-layout';
 import { validateStyleProps, validStyle } from './utils/valid-style';
 
-export { Box, validStyle, validateStyleProps}
+export { Box, validStyle, validateStyleProps, AnimatedBox, BoxAnimatedLayout }
 export type { BoxProps, BoxStyle }
